eb11def5160f322359df5d328973c7afb130cac3
Ubugeeei <ubuge1122@gamil.com>
2022-08-15 21:58:56 +0900

    feat: canvas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("panel-pop", 800, 600)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    'mainloop: loop {
+        for event in sdl_context.event_pump()?.poll_iter() {
+            match event {
+                // Quit if the window is closed
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Option::Some(Keycode::Escape),
+                    ..
+                } => break 'mainloop,
+
+                // change background color
+                Event::KeyUp {
+                    keycode: Option::Some(Keycode::Space),
+                    ..
+                } => {
+                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+                    canvas.clear();
+                    canvas.present();
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
